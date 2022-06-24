736b69d1b025f0bafb957869da5e997cdbd2823c
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2022-06-24 17:19:04 +0600

    Initial

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application } from 'express'
+const bodyParser = require('body-parser')
+
+const app: Application = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+const port = 8080
+app.listen(port, () => console.log(`Running on port ${port}`))
