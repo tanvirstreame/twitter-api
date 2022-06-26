e92b4224b40f3e4dc6befc1c36eb3fbc18eeea76
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2022-06-27 01:44:23 +0600

    Added self follow validation with error handling

--- src/follow/follow.service.ts
+++ src/follow/follow.service.ts
@@ -2,12 +2,25 @@ export { };
 const { model } = require("mongoose");
 const Follow = model("Follow");
 const User = model("User");
+const ObjectId = require('mongodb').ObjectID;
 
 exports.addFollow = async (data: any) => {
 
 	/**
 	 * Add follow here
 	 */
+
+	if(ObjectId(data?.follower).valueOf() === String(data?.followee)) {
+
+		/**
+		 * If any user try to follow himself
+		 */
+
+		let error: any = new Error("You can't follow yourself, try another followee");
+		error.status = 400;
+		throw error;
+
+	}
 
 	const followInfo = await Follow.findOne(data, ["_id"]).lean();
 	if (followInfo?._id) {
