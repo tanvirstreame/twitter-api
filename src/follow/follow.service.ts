7e7be63359be7dcc7aa814bd181b04bcd0e73cdc
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2022-06-27 01:12:55 +0600

    Validating followee id and show proper errors

--- src/follow/follow.service.ts
+++ src/follow/follow.service.ts
@@ -1,19 +1,39 @@
 export { };
 const { model } = require("mongoose");
 const Follow = model("Follow");
+const User = model("User");
 
 exports.addFollow = async (data: any) => {
-	
+
 	/**
 	 * Add follow here
 	 */
 
-	const followInfo =  await Follow.findOne(data, ["_id"]).lean();
-	if(followInfo?._id) {
-		throw new Error("You are already following this person");
+	const followInfo = await Follow.findOne(data, ["_id"]).lean();
+	if (followInfo?._id) {
+
+		/**
+		 * Alraedy following the followee
+		 */
+
+		let error: any = new Error("You are already following this person");
+		error.status = 400;
+		throw error;
 	}
 	else {
-		return await Follow.create(data);
+		const followInfo = await User.findOne({ _id: data?.followee }, ["_id"]).lean();
+		if (followInfo?._id) {
+
+			/**
+			 * Validating if the id really exists
+			 */
+
+			return await Follow.create(data);
+		}
+		else {
+			let error: any = new Error("No such followee with that id");
+			error.status = 404;
+			throw error;
+		}
 	}
-	
 }
